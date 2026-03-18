9c813787fd51824006ac2edecb51eecafe208b2c
gtcJeremy <jsanche3073@student.gwinnetttech.edu>
2026-03-17 21:26:39 -0400

    Add files via upload
    this is a proposed update to two pre existing scripts namely: Gun.cs and island movement.cs the verison of unity used to make this was done in 1.17 and i will try and download the current version we are using but for now here's what i've been working on.

---
+++ Assets/Scripts/GunMechanic.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required for accessing UI components
+
+// IMPORTANT NOTE:
+// This code and cs was done in unity 6000.1.17 as such it will be uploaded in different script than the actual gun.cs acript
+// Since where switching to a different unity engine I will be downloading the new one we are using.
+// Also this uses inputed key and it should be changed in player input manager to be using both.
+
+
+public class Gun : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10;
+
+    // New variables for ammo management
+    public int maxAmmo = 30; // Maximum bullets the gun can hold
+    private int currentAmmo; // Current remaining bullets
+    public Text ammoText; // Reference to the UI Text element
+
+    void Start()
+    {
+        currentAmmo = maxAmmo; // Initialize current ammo on scene start
+        UpdateAmmoUI(); // Update UI immediately
+    }
+
+    void Update()
+    {
+        // Change from GetKeyDown to GetButtonDown (or another GetKey, up to you) for continuous holding/shooting if desired
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Check if there is enough ammo to shoot
+            if (currentAmmo > 0)
+            {
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        // Instantiate the bullet
+        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        bullet.GetComponent<Rigidbody>().linearVelocity = bulletSpawnPoint.forward * bulletSpeed;
+
+        // Waste a bullet (decrement ammo count)
+        currentAmmo--;
+
+        // Update the UI text
+        UpdateAmmoUI();
+    }
+
+    void UpdateAmmoUI()
+    {
+        // Display current ammo / max ammo
+        if (ammoText != null)
+        {
+            ammoText.text = "Ammo: " + currentAmmo.ToString() + " / " + maxAmmo.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/IslandMovementEditIdea.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// IMPORTANT NOTE:
+// This code and cs was done in unity 6000.1.17 as such it will be uploaded in different script than the actual IslandMovement.cs acript
+// Since where switching to a different unity engine I will be downloading the new one we are using.
+// Also this uses inputed key and it should be changed in player input manager to be using both.
+
+public class IslandMove : MonoBehaviour
+{
+    [Header("Movement Speeds (units per second)")]
+    public float speedUp = 2.0f;
+    public float speedDown = 2.0f;
+    public float speedLeft = 2.0f;
+    public float speedRight = 2.0f;
+
+    [Header("Enable/Disable Directions")]
+    public bool canMoveUp = true;
+    public bool canMoveDown = true;
+    public bool canMoveLeft = true;
+    public bool canMoveRight = true;
+
+    // You can remove pointA, pointB, speed, and moveTowardsB from the original script
+    // if they are no longer needed.
+    // public Transform pointA;
+    // public Transform pointB;
+    // private bool moveTowardsB = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initial setup if needed
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementVector = Vector3.zero;
+
+        // Calculate movement based on enabled directions and specific speeds
+        if (canMoveUp)
+        {
+            // Move up (positive Y axis)
+            movementVector += Vector3.up * speedUp;
+        }
+        if (canMoveDown)
+        {
+            // Move down (negative Y axis)
+            movementVector += Vector3.down * speedDown;
+        }
+        if (canMoveRight)
+        {
+            // Move right (positive X axis)
+            movementVector += Vector3.right * speedRight;
+        }
+        if (canMoveLeft)
+        {
+            // Move left (negative X axis)
+            movementVector += Vector3.left * speedLeft;
+        }
+
+        // Apply movement scaled by Time.deltaTime to ensure consistent speed across different frame rates
+        // The default space is local, which is usually fine, but can be specified to Space.World if needed
+        transform.Translate(movementVector * Time.deltaTime, Space.World);
+    }
+}
